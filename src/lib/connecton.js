86bc9ebac243c5da38647077935e9410e85a7095
rahbar12 <rahbar.img@gmail.com>
2023-03-28 09:19:51 +0530

    db connection

--- src/lib/connecton.js
+++ src/lib/connecton.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 //  mongoose.set('debug', true);
 
-mongoose.connect("mongodb://127.0.0.1:27017/buttress",{
+mongoose.connect(`${process.env.URI}${process.env.DB_NAME}`,{
     
     useNewUrlParser:true,
 
